e5f7d17d3589c245f434d6e06289b901b3a75df3
Bart Massey <bart.massey@gmail.com>
2021-05-20 02:34:06 -0700

    added async client for high-speed testing

---
+++ src/bin/ccasync/main.rs
@@ -0,0 +1,26 @@
+use async_std::net::*;
+use async_std::prelude::*;
+use async_std::task;
+
+async fn get_count() -> u64 {
+    let mut stream = TcpStream::connect("127.0.0.1:10123").await.unwrap();
+    let mut buf = String::with_capacity(22);
+    stream.read_to_string(&mut buf).await.unwrap();
+    buf.trim_end().parse().unwrap()
+}
+
+
+async fn send(n: usize) {
+    let mut handles = Vec::with_capacity(n);
+    for _ in 0..n {
+        handles.push(task::spawn(get_count()));
+    }
+    for h in handles {
+        println!("{}", h.await);
+    }
+}
+
+fn main() {
+    let n = std::env::args().nth(1).unwrap().parse().unwrap();
+    task::block_on(send(n));
+}
