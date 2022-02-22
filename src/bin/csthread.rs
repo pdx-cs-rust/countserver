f511c0a4a2ef7a198aa0ee727f019d6ff3846c5e
Bart Massey <bart.massey@gmail.com>
2022-02-21 16:14:31 -0800

    fixed csthread to eventually reap children (requires nightly)

--- src/bin/csthread.rs
+++ src/bin/csthread.rs
@@ -1,3 +1,5 @@
+#![feature(thread_is_running)]
+
 use std::io::Write;
 use std::net::*;
 use std::sync::{
@@ -8,15 +10,30 @@ use std::sync::{
 fn main() {
     let counter = Arc::new(AtomicU64::new(0));
     let listener = TcpListener::bind("127.0.0.1:10123").unwrap();
+    let mut children = Vec::new();
     loop {
         let (mut socket, _addr) = listener.accept().unwrap();
         //eprintln!("new client: {:?}", addr);
         let counter = Arc::clone(&counter);
-        let _ = std::thread::spawn(move || {
+        let handle = std::thread::spawn(move || {
             let count = counter.fetch_add(1, Ordering::SeqCst);
             write!(socket, "{}\r\n", count).unwrap();
             socket.flush().unwrap();
             drop(socket);
         });
+        children.push(handle);
+        if children.len() >= 100 {
+            children = children
+                .into_iter()
+                .filter_map(|h| {
+                    if h.is_running() {
+                        Some(h)
+                    } else {
+                        let () = h.join().unwrap();
+                        None
+                    }
+                })
+                .collect();
+        }
     }
 }
