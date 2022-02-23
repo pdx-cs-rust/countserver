8a25d6ad2e0c4341e340707de29723e6d039c60b
Bart Massey <bart.massey@gmail.com>
2022-02-23 15:47:03 -0800

    restructured ccasync to work reliably

--- src/bin/ccasync.rs
+++ src/bin/ccasync.rs
@@ -1,13 +1,13 @@
 #[cfg(feature = "async-std-rt")]
 mod async_rt {
-    pub use async_std::io::{self, ReadExt};
+    pub use async_std::io::{self, ReadExt, WriteExt};
     pub use async_std::net::TcpStream;
     pub use async_std::task;
 }
 
 #[cfg(feature = "tokio-rt")]
 mod async_rt {
-    pub use tokio::io::{self, AsyncReadExt};
+    pub use tokio::io::{self, AsyncReadExt, AsyncWriteExt};
     pub use tokio::net::TcpStream;
     pub use tokio::task;
     pub use tokio::runtime::Runtime;
@@ -19,17 +19,21 @@ async fn get_count() -> u64 {
     let mut stream = TcpStream::connect("127.0.0.1:10123").await.unwrap();
     let mut buf = String::with_capacity(22);
     stream.read_to_string(&mut buf).await.unwrap();
+    drop(stream);
     buf.trim_end().parse().unwrap()
 }
 
-
 async fn send(n: usize) {
-    let mut handles = Vec::with_capacity(n);
     for _ in 0..n {
-        handles.push(task::spawn(get_count()));
-    }
-    for h in handles {
-        println!("{:?}", h.await);
+        let _status = task::spawn(async {
+            let c = get_count().await;
+            io::stdout()
+                .write_all(format!("{}\n", c).as_bytes())
+                .await
+                .unwrap();
+        }).await;
+        #[cfg(feature = "tokio-rt")]
+        _status.unwrap();
     }
 }
 
