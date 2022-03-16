993be7e988594ba4c8179e71fd280a743ad34b58
Bart Massey <bart.massey@gmail.com>
2022-03-16 10:33:32 -0700

    replaced seq server counter with faster impl

--- src/ccthread.rs
+++ src/ccthread.rs
@@ -1,3 +1,4 @@
+use std::collections::VecDeque;
 use std::io::Read;
 use std::net;
 use std::thread;
@@ -10,16 +11,16 @@ fn get_count() -> u64 {
 }
 
 pub fn send(n: usize, m: usize) {
-    let mut handles = Vec::with_capacity(m);
+    let mut handles = VecDeque::with_capacity(2 * m);
     for _ in 0..n {
-        handles.push(thread::spawn(get_count));
-        if handles.len() >= m {
-            for h in handles.drain(..) {
+        handles.push_front(thread::spawn(get_count));
+        if handles.len() > m {
+            for h in handles.drain(m..) {
                 println!("{:?}", h.join().unwrap());
             }
         }
     }
-    for h in handles {
+    for h in handles.drain(..) {
         println!("{:?}", h.join().unwrap());
     }
 }
--- src/csthread.rs
+++ src/csthread.rs
@@ -1,36 +1,26 @@
 use std::io::Write;
+use std::collections::VecDeque;
 use std::net::*;
 use std::sync::atomic::{AtomicU64, Ordering};
 
 pub fn start(m: usize) {
     static COUNTER: AtomicU64 = AtomicU64::new(0);
     let listener = TcpListener::bind("127.0.0.1:10123").unwrap();
-    let mut children = Vec::new();
+    let mut handles = VecDeque::with_capacity(2 * m);
     loop {
         let (mut socket, _addr) = listener.accept().unwrap();
         let counter = &COUNTER;
-        let handle = std::thread::spawn(move || {
+        let h = std::thread::spawn(move || {
             let count = counter.fetch_add(1, Ordering::SeqCst);
             write!(socket, "{}\r\n", count).unwrap();
             socket.flush().unwrap();
             drop(socket);
         });
-        children.push(handle);
-        // XXX Clippy false-positive on the `filter_map()`. See
-        // https://github.com/rust-lang/rust-clippy/issues/4433
-        #[allow(clippy::unnecessary_filter_map)]
-        while children.len() >= m {
-            children = children
-                .into_iter()
-                .filter_map(|h| {
-                    if !h.is_finished() {
-                        Some(h)
-                    } else {
-                        let () = h.join().unwrap();
-                        None
-                    }
-                })
-                .collect();
+        handles.push_front(h);
+        if handles.len() > m {
+            for h in handles.drain(m..) {
+                h.join().unwrap();
+            }
         }
     }
 }
